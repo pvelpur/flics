ebc542d5ca8842e1928eddebddbf4a65f5677584
Dhruv Aggarwal <dhruv@Dhruvs-MacBook-Pro.local>
2020-11-21 18:02:28 -0500

    Setting up Dhruv Branch

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,6 +5,6 @@ const { signup } = require('../controllers/auth')
 const router = express.Router()
 
 router.post('/signup', signup)
-
+router.get('/',(req,res)=>{res.send("hello world")})
 module.exports = router
 
