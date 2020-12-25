b74074f5d65c5bf2dbea3e8130c6f80bca676d7c
Dhruv Aggarwal <daggarwa@localhost.localdomain>
2020-12-24 19:16:42 -0500

    Added a functioning dashboard

--- Front-end/flics/src/core/Dashboard.js
+++ Front-end/flics/src/core/Dashboard.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {signout as signoutAction} from '../actions'
 import {useDispatch} from 'react-redux'
 import {useHistory} from 'react-router-dom'
-
+import Mygroups from './Mygroups'
+import Mylist from './Mylist'
+import Profile from './Profile'
 function Dashboard() {
     const dispatch = useDispatch()
     const history = useHistory()
-    
+    const [activeToggle,setActiveToggle] = useState('Mygroups')
     const signout = (next) => {
         dispatch(signoutAction())
         next() //redirect the user
@@ -22,10 +24,23 @@ function Dashboard() {
 
     return (
         <div className='container'>
-            This is the Dashboard <br />
-            Todo: Only allow entry if logged in!
-            <br />
-            <button type="button" className="btn btn-warning" onClick={()=>signout(()=> history.push('/login'))}>Click ME!!!</button>
+            <Profile/>
+            <div className='mx-auto' style={{width: "600px"}}>
+                <div className="btn-group">
+                    <div className="row mx-auto">
+                        <div className="col" >
+                        <button type="button" className={activeToggle === 'Mygroups'? "btn btn-outline-success":"btn btn-outline-dark"} onClick={()=>setActiveToggle('Mygroups')}>My Groups</button>
+                        </div>
+                        <div className="col">
+                        <button type="button" className={activeToggle === 'Mylist'? "btn btn-outline-success":"btn btn-outline-dark"} onClick={()=>setActiveToggle('Mylist')}>My List</button>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    {activeToggle === 'Mygroups'?<Mygroups />: <Mylist />}               
+                </div>
+                <button type="button" className="btn btn-warning" onClick={()=>signout(()=> history.push('/login'))}>Sign Out!!!</button>            
+            </div>
         </div>
     )
 }
