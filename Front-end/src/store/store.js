d9580cdfcdf783d4617f8623be8a9ef27cb4ef83
pvelpur <pvelpur@purdue.edu>
2021-01-25 23:28:11 -0600

    trying to fix prod issue

--- Front-end/src/store/store.js
+++ Front-end/src/store/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from 'redux'
 import { persistStore } from 'redux-persist'
 import allReducers from '../reducers'
 import thunk from 'redux-thunk'
-import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const store = createStore(
   allReducers,
