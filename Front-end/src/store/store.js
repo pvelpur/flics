a5c8e3e627facc71209854865d6288f8da3fab1c
pvelpur <pvelpur@purdue.edu>
2021-01-25 22:11:46 -0600

    production environment removing redux devtools

--- Front-end/src/store/store.js
+++ Front-end/src/store/store.js
@@ -2,11 +2,12 @@ import { createStore, applyMiddleware } from 'redux'
 import { persistStore } from 'redux-persist'
 import allReducers from '../reducers'
 import thunk from 'redux-thunk'
-import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
+//import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 
 const store = createStore(
   allReducers,
-  composeWithDevTools(applyMiddleware(thunk))
+  applyMiddleware(thunk)
+  //composeWithDevTools(applyMiddleware(thunk))
 )
 
 const persistor = persistStore(store)
