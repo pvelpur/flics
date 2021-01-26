07b2b54f2e0c2b33b0b4bf3bcf7926f3d2fa0666
pvelpur <pvelpur@purdue.edu>
2021-01-25 21:59:28 -0600

    redux devtools only in development + node version in package.json

--- Front-end/src/store/store.js
+++ Front-end/src/store/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from 'redux'
 import { persistStore } from 'redux-persist'
 import allReducers from '../reducers'
 import thunk from 'redux-thunk'
-import { composeWithDevTools } from "redux-devtools-extension";
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 
 const store = createStore(
   allReducers,
