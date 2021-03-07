b54368f6b5c0f4d9ec5fd867a666aa94f85d4d36
pvelpur <pvelpur@purdue.edu>
2021-03-06 22:14:37 -0600

    still trying to figure out proxy stuff :(

--- Front-end/src/setupProxy.js
+++ Front-end/src/setupProxy.js
@@ -4,8 +4,15 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:8080/',
+      target: 'http://localhost:8080',
       changeOrigin: true,
     })
   );
+  app.use(
+    '/api/groups',
+    createProxyMiddleware({
+      target: 'http://localhost:8080',
+      changeOrigin:true
+    })
+  )
 };
